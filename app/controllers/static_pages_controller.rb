1a6d7551414795d5046a4dbe059984cc49c050cf
Ozim <lukasz.ozimek@netguru.com>
2019-05-17 13:35:41 +0200

    Update homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,9 @@
 class StaticPagesController < ApplicationController
   def home
     environment = ENV.fetch("RAILS_ENV")
-    branch = `git rev-parse --abbrev-ref HEAD`
-    app_version = `git describe --tag`
 
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
     changelog = markdown.render(File.read("#{Rails.root}/CHANGELOG.md"))
-    render locals: { environment: environment, branch: branch, version: app_version, changelog: changelog }
+    render locals: { environment: environment, changelog: changelog }
   end
 end
